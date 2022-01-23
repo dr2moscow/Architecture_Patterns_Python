b51154e653b240d27fe7dfc980fe5ebbb7b03b02
dr2moscow <dr2moscow@gmail.com>
2022-01-23 20:22:25 +0300

    ДЗ 1

---
+++ catalog.py
@@ -0,0 +1,18 @@
+flowers = [
+    {
+        'name': 'Rose',
+        'color': 'Red'
+    },
+    {
+        'name': 'Rose',
+        'color': 'Blue'
+    },
+    {
+        'name': 'Tulip',
+        'color': 'White'
+    },
+    {
+        'name': 'Tulip',
+        'color': 'Yellow'
+    },
+]
