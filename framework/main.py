211550bf3f593f00b45d5dec05c9f4baa64bdce0
dr2moscow <dr2moscow@gmail.com>
2022-02-05 19:27:49 +0300

    ДЗ 4

--- framework/main.py
+++ framework/main.py
@@ -15,8 +15,8 @@ class Framework:
         self.fronts_lst = fronts_obj
 
     def __call__(self, environ, start_response):
-        # получаем адрес, по которому выполнен переход в браузере
 
+        # получаем адрес, по которому выполнен переход в браузере
         path = environ['PATH_INFO']
 
         # добавление закрывающего слеша
@@ -25,6 +25,7 @@ class Framework:
 
         # обрабатыаем запросы
         request = {}
+
         # Получаем все данные запроса
         method = environ['REQUEST_METHOD']
         request['method'] = method
@@ -33,20 +34,18 @@ class Framework:
             data = PostRequests().get_request_params(environ)
             request['data'] = Framework.decode_value(data)
             print(f'Нам пришёл post-запрос: {Framework.decode_value(data)}')
+            print(f'Печатаем {data}')
         if method == 'GET':
             request_params = GetRequests().get_request_params(environ)
             request['request_params'] = Framework.decode_value(request_params)
-            print(f'Нам пришли GET-параметры:'
-                  f' {Framework.decode_value(request_params)}')
+            print(f'Нам пришли GET-параметры: {Framework.decode_value(request_params)}')
 
         # находим нужный контролер
         # отработка патерна PC
-
         if path in self.routes_lst:
             view = self.routes_lst[path]
         else:
             view = PageNotFound404()
-        request = {}
 
         # наполняем словарь requuest элементами
         # этот словарь получат все контролеры
@@ -67,3 +66,4 @@ class Framework:
             val_decode_str = decodestring(val).decode('UTF-8')
             new_data[k] = val_decode_str
         return new_data
+
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from datetime import date
-from views import Index, About, Contact
+from views import Index, About, Contact, StudyPrograms, CoursesList, \
+    CreateCourse, CreateCategory, CategoryList, CopyCourse
 
 
 # front controller
@@ -17,4 +18,10 @@ routes = {
     '/': Index(),
     '/about/': About(),
     '/contact/': Contact(),
+    '/study_programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse()
 }
